b435e04f68266bbaf2fcd4796bda1ea4d0f1e83e
Diogo Queiroz <diogo.qds97@gmail.com>
2020-09-27 15:16:02 -0300

    create task to register friends

---
+++ lib/mix/tasks/utils/add_fake_friends.ex
@@ -0,0 +1,30 @@
+defmodule Mix.Tasks.Utils.AddFakeFriends do
+  use Mix.Task
+  alias NimbleCSV.RFC4180, as: CSVParser
+
+  @shortdoc "Add Fake Friends on App"
+  def run(_) do
+    Faker.start()
+    create_friends([], 50)
+    |> CSVParser.dump_to_iodata()
+    |> save_csv_file()
+
+    IO.puts "Amigos cadastrados com sucesso!"
+  end
+
+  defp create_friends(list, 1) do
+    list ++ [random_list_friend()]
+  end
+
+  defp create_friends(list, count) do
+    list ++ [random_list_friend()] ++ create_friends(list, count - 1)
+  end
+
+  defp random_list_friend do
+    [Faker.Person.PtBr.name(), Faker.Internet.email(), Faker.Phone.EnUs.phone()]
+  end
+
+  defp save_csv_file(data) do
+    File.write!("#{File.cwd!}/friends.csv", data, [:append])
+  end
+end
